3ef13d8814c20b7265eab0ee4167190424591b0d
niranjanrai <rainiranjan1998@gmail.com>
2022-11-19 18:41:29 +0530

    first commit

--- script.js
+++ script.js
@@ -77,37 +77,31 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
-
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = ` ${balance} INR `;
 };
 
-calcDisplayBalance(account1.movements);
-
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = ` ${incomes}₹`;
 
-  const out = movements
+  const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumOut.textContent = `${Math.abs(out)}₹`;
 
-  const interest = movements
+  const interest = acc.movements
     .filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 1);
   labelSumInterest.innerHTML = `${interest}₹`;
 };
-
-calcDisplaySummary(account1.movements);
 
 const user = 'Manoj saroj'; // ms
 
@@ -125,11 +119,35 @@ const createUsernames = function (accs) {
 createUsernames(accounts);
 
 // Event handlers
-let currentUser = btnLogin.addEventListener('click', function (e) {
+let currentAccount = btnLogin.addEventListener('click', function (e) {
   e.preventDefault();
 
-  currentUser = accounts.find(acc => acc.username === inputLoginUsername.value);
-  console.log(currentUser);
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // welcome message and UI Display
+    console.log('LOGIN');
+    labelWelcome.textContent = `Welcome back ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+    // Clear Inputfeilds
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Display movement
+    displayMovements(currentAccount.movements);
+
+    // Display balance
+
+    calcDisplayBalance(currentAccount.movements);
+
+    // Display summary
+    calcDisplaySummary(currentAccount);
+  }
 });
 
 /////////////////////////////////////////////////
