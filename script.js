01e7ee2473717a376618aad27f5ef9b6ee2451b7
niranjanrai <rainiranjan1998@gmail.com>
2022-11-22 20:19:36 +0530

    first commit

--- script.js
+++ script.js
@@ -10,6 +10,18 @@ const account1 = {
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
 };
 
 const account2 = {
@@ -17,23 +29,35 @@ const account2 = {
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
 };
 
-const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+// const account3 = {
+//   owner: 'Steven Thomas Williams',
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
 
-const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// const account4 = {
+//   owner: 'Sarah Smith',
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [account1, account2];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -71,7 +95,7 @@ const displayMovements = function (movements, sort = false) {
     const html = `
   <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-    <div class="movements__value"> ${mov}₹ </div>
+    <div class="movements__value"> ${mov.toFixed(2)}₹ </div>
   </div>
   `;
     containerMovements.insertAdjacentHTML('afterbegin', html);
@@ -81,19 +105,19 @@ const displayMovements = function (movements, sort = false) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = ` ${acc.balance} INR `;
+  labelBalance.textContent = ` ${acc.balance.toFixed(2)} INR `;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = ` ${incomes}₹`;
+  labelSumIn.textContent = ` ${incomes.toFixed(2)}₹`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}₹`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}₹`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -102,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 1);
-  labelSumInterest.innerHTML = `${interest}₹`;
+  labelSumInterest.innerHTML = `${interest.toFixed(2)}₹`;
 };
 
 const user = 'Manoj saroj'; // ms
@@ -182,7 +206,9 @@ btnTransfer.addEventListener('click', function (e) {
 
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputLoanAmount.value);
+
+  const amount = Math.floor(inputLoanAmount.value);
+
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add the movement
     currentAccount.movements.push(amount);
@@ -602,13 +628,13 @@ console.log(movements.sort((a, b) => a - b));
 console.log(movements.sort((a, b) => b - a));
 console.log(movements.sort((a, b) => console.log('hie')));
 
-*/
+
 
 console.log([1, 2, 3, 4, 5, 6, 7, 8]);
 console.log(new Array(1, 2, 3, 4, 5, 6, 7, 8));
 
 const x = new Array(7);
-// console.log(x.fill(5));
+console.log(x.fill(5));
 console.log(x.fill(5, 4, 6));
 
 console.log(x);
@@ -626,3 +652,28 @@ labelBalance.addEventListener('click', function () {
 
   console.log(movementsUI2);
 });
+
+*/
+
+console.log(5 % 2);
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach((row, i) => {
+    if (i === 0) {
+      row.style.backgroundColor = 'orange';
+    }
+    if (i === 1) {
+      row.style.backgroundColor = 'white';
+    }
+    if (i === 2) {
+      row.style.backgroundColor = 'green';
+    }
+    // console.log(i);
+  });
+});
+
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(23 / 0);
+console.log(545864354357657513576574538756434567687543546878354354567n);
+console.log(BigInt(545864354357657513576574538756434567687543546878354354567));
