3d25825729e23c7d3780f3581bf039c675258480
niranjanrai <rainiranjan1998@gmail.com>
2022-11-27 17:38:23 +0530

    first commit

--- script.js
+++ script.js
@@ -725,3 +725,7 @@ console.log(2 ** 53 + 1);
 console.log(2 ** 53 + 2);
 console.log(2 ** 53 + 3);
 console.log(2 ** 53 + 4);
+
+document.querySelector('.logo').addEventListener('click', () => {
+  alert('You clicked me!!');
+});
