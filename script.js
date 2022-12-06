ff79af5fe77d7e9aa9fb6f2c31488b3d16368df1
niranjanrai <rainiranjan1998@gmail.com>
2022-12-06 23:46:10 +0530

    first cmmit

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ const account3 = {
 };
 
 const account4 = {
-  owner: 'Shubham sharaff',
+  owner: 'Shubham Sharaff',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
   pin: 4444,
@@ -81,7 +81,7 @@ const account4 = {
   locale: 'en-US',
 };
 
-const accounts = [account1, account2];
+const accounts = [account1, account2, account3, account4];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -127,6 +127,14 @@ const formatMovementDate = function (date, locale) {
   return new Intl.DateTimeFormat(locale).format(date);
 };
 
+const formatCur = function (value, locale, currency) {
+  //
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency: currency,
+  }).format(value);
+};
+
 const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
@@ -140,10 +148,7 @@ const displayMovements = function (acc, sort = false) {
     const date = new Date(acc.movementsDates[i]);
     const displayDate = formatMovementDate(date, acc.locale);
 
-    const formattedMov = new Intl.NumberFormat(acc.locale, {
-      style: 'currency',
-      currency: acc.currency,
-    }).format(mov);
+    const formattedMov = formatCur(mov, acc.locale, acc.currency);
 
     const html = `
   <div class="movements__row">
@@ -159,19 +164,19 @@ const displayMovements = function (acc, sort = false) {
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = ` ${acc.balance.toFixed(2)} INR `;
+  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = ` ${incomes.toFixed(2)}₹`;
+  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}₹`;
+  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -180,7 +185,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 1);
-  labelSumInterest.innerHTML = `${interest.toFixed(2)}₹`;
+  labelSumInterest.innerHTML = formatCur(interest, acc.locale, acc.currency);
 };
 
 const user = 'Manoj saroj'; // ms
@@ -361,7 +366,8 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 const num = 12453213.454;
 
-// console.log(`${new Intl.NumberFormat('en-UK').format(num)}`);
+/* 
+console.log(`${new Intl.NumberFormat('en-UK').format(num)}`);
 const options = {
   style: 'currency',
   unit: 'celsius',
@@ -371,3 +377,8 @@ console.log(new Intl.NumberFormat(navigator.language, options).format(num));
 
 console.log(new Intl.NumberFormat('en-US', options).format(num));
 console.log(new Intl.NumberFormat('de-DE', options).format(num));
+
+*/
+
+setTimeout(() => console.log('here is your pizza'), 3000);
+// setInterval(() => console.log('here is your pizza'), 3000);
